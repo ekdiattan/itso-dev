5057619faa4a7cc9b4f163ed8961476c573a16b0
Hadyan Yuma Ekdiattan <hadyanyuma.sekawanmedia@gmail.com>
2024-08-22 18:47:10 +0700

    adjust

--- resources/views/home/aset/booking/reject.blade.php
+++ resources/views/home/aset/booking/reject.blade.php
@@ -4,12 +4,8 @@
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
     <h2>Peminjaman Aset</h2>
 </div>
-
-<!-- double navbar -->
 <div class="nav navbar navbar-expand navbar-white navbar-light border-bottom p-0">
-    <!-- Container wrapper -->
     <div class="container justify-content-center justify-content-md-between">
-      <!-- Left links -->
       <ul class="navbar-nav flex-row">
         <li class="nav-item me-auto">
           <a class="nav-link" href="/booking" style="color:rgb(250, 162, 0);font-size:20px;" role="button" data-mdb-toggle="sidenav" data-mdb-target="#sidenav-1"
@@ -38,11 +34,6 @@
       </ul>
     </div>
 </div>
-@if(session('success'))
-<div class="alert alert-success" role="alert">
-  {{ session('success') }}
-</div>
-@endif
 <div class="col-lg-12 grid-margin stretch-card">
         <br>
         <div class="card">
@@ -50,12 +41,6 @@
                 <h4 class="card-title"><b>Ditolak</b></h4>
                 <div class="table-responsive">
                  <nav class="navbar bg-body-tertiary">
-                    <!-- <form action="#" method="GET">
-                        <form class="d-flex" role="search">
-                            <input class="form-control me-2" type="search" placeholder="Masukan Nama Pemohon" aria-label="Search" name="search" value="">
-                        </form>
-                    <a href="#" class="btn btn-danger" role="button">Reset</a> -->
-                    </form>
                  </nav>
                 <table id="dataTable" class="table table-hover table-bordered table-striped">
                         <thead class="bg-gray disabled color-palette">
