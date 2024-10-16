81c6bc41b53684d6d55cf0368320361434b62975
Hadyan Yuma Ekdiattan <hadyanyuma.sekawanmedia@gmail.com>
2024-10-16 21:55:19 +0700

    add user sql

---
+++ database/sql/User.sql
@@ -0,0 +1,3 @@
+INSERT INTO 'User' ('UserId', 'name', 'password', 'UserCreatedAt', 'UserUpdatedAt', 'UserDeletedAt', 'UserCreatedBy', 'UserUpdatedBy', 'UserDeletedBy', 'UserEmployeeId', 'UserRoleId')
+VALUES 
+    (8, 'tfuprpifqj', '$2y$12$OQ0L2xeeMgGKhfA5JA1o1emhrODQhdpeaKjODUpGo6L3iY91aSPCu', NOW() AT TIME ZONE 'Asia/Jakarta',  NOW() AT TIME ZONE 'Asia/Jakarta', NULL, NULL, NULL, NULL, 8, 1);
